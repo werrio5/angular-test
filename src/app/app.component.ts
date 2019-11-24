7e3e21890aa4d4cf487ec791dfc16033bd21d167
werrio5 <pavel.werrio5@gmail.com>
2019-11-24 21:21:56 +0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,26 @@ export class AppComponent  {
   title = '';
 
   constructor(private headerService: HeaderService) {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');  
+      if(token!=null) 
+        this.headerService.setTitle('Hello, '+username+'!');
+        else
      this.headerService.setTitle('Hello, guest!');
   }
 
-  ngOnInit() {
+  ngOnInit() {     
+
     this.headerService.title.subscribe(title => {
-      this.title = title;
+     this.title = title;
     });
   }
 
+logOut(){
+  localStorage.removeItem('token');
+  localStorage.removeItem('username');
+  this.headerService.setTitle('Hello, guest!');
+}
 
 } 
 
