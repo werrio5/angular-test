7e0be2b6bee994f0d800d1a290271898d7e8089c
werrio5 <pavel.werrio5@gmail.com>
2019-11-24 13:08:23 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { HeaderService } from './services/header.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ] 
+  
 })
 
 
