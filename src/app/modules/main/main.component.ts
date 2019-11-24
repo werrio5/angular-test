23d73e3b5c2f59786018b6a8ad1cdb87523e7333
werrio5 <pavel.werrio5@gmail.com>
2019-11-24 14:14:20 +0200

    5 files modified

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -12,4 +12,5 @@ export class MainComponent implements OnInit {
   ngOnInit() {
   }
 
+fetchData = [{"title":"saurabh","description":"dd","tagline":"tt","date":"dd"},{"title":"aman","description":"dd","tagline":"tt","date":"dd"},{"title":"jessica","description":"dd","tagline":"tt","date":"dd"},{"title":"rosh","description":"dd","tagline":"tt","date":"dd"}];
 }
--- src/app/modules/root/root.component.ts
+++ src/app/modules/root/root.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css']
 })
+
+
 export class RootComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+fetchData = [{"title":"saurabh","description":"dd","tagline":"tt","date":"dd"},{"title":"aman","description":"dd","tagline":"tt","date":"dd"},{"title":"jessica","description":"dd","tagline":"tt","date":"dd"},{"title":"rosh","description":"dd","tagline":"tt","date":"dd"}];
 
 }
